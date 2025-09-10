9d63c4028003e7f484c171b4ede6396f334bf296
Raj Rai <rairaj1411@gmail.com>
2025-09-10 10:20:45 +0530

    contact model is saved

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    First_Name = models.CharField(max_length=125)
+    Last_Name = models.CharField(max_length=125)
+    Email = models.CharField(max_length=125)
+    PhoneNumber = models.CharField(max_length=12)
+    City = models.CharField(max_length=100)
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('P', 'Princess'),
+        ('O', 'Other'),
+    ]
+    Gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    Goals = models.TextField(max_length=125)
+    
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
+ 
 from django.shortcuts import render, HttpResponse
+
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -11,4 +14,15 @@ def about(request):
     return render(request,'about.html')
 
 def contacts(request):
+    if request.method=="POST":
+        First_Name = request.POST.get('First_Name')
+        Last_Name = request.POST.get('Last_Name')
+        Email = request.POST.get('Email')
+        PhoneNumber = request.POST.get('PhoneNumber')
+        City = request.POST.get('City')
+        Gender = request.POST.get('Gender')
+        Goals = request.POST.get('Goals')
+        contact=Contact(First_Name=First_Name,Last_Name=Last_Name, Email=Email,
+                        PhoneNumber=PhoneNumber, City=City, Gender=Gender, Goals=Goals)
+        contact.save()
     return render(request,'contacts.html')
